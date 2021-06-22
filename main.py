56a4d12ab02da66d6d07762f865dcc8cebeb5b79
yellowHSE <74814515+yellowHSE@users.noreply.github.com>
2021-06-23 08:04:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 
 #DB 값 설정
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'yellow2090*'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'ospteamproject9'
 app.config['MYSQL_DATABASE_DB'] = 'reviewdb'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.secret_key = "ABCDEFG"
